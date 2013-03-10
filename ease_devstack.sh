5840cb77a1838cc6fbdb96978024fcc603bbca44
Rushi Agrawal <rushi.agr@gmail.com>
2013-03-10 20:29:58 +0530

    changed ease_devstack to use all dev/pts/*

--- ease_devstack.sh
+++ ease_devstack.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-echo "chown stack:stack `readlink /proc/self/fd/0`" >> /root/.bashrc
+echo "chown stack:stack `readlink /proc/self/fd/*`" >> /root/.bashrc
 echo ". /opt/stack/devstack/eucarc" >> /opt/stack/.bashrc
