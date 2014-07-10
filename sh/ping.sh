21210efc3ebf1d0171de49132e4469667e4b5e12
Rushi Agrawal <rushi.agr@gmail.com>
2014-07-10 20:43:57 +0530

    Add attribution

--- sh/ping.sh
+++ sh/ping.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# NOTE: borrowed from https://askubuntu.com/questions/413367/ping-multiple-ips-using-bash/413378#413378
+
 for i in `seq ${2} ${3}`
 do
     ping -c 1 ${1}.${i} > /dev/null 2>&1
