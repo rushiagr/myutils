fd8f22182d681b9094f40a8d477e5f1c9a2c6f68
Rushi Agrawal <rushi.agr@gmail.com>
2014-07-29 22:36:25 +0530

    install-aliases.sh actually sets up aliases in any system.
    Also, this commit includes first steps towards making the VPN
    script as a bash function.

--- install-aliases.sh
+++ install-aliases.sh
@@ -2,10 +2,21 @@
 
 # Script which sets everthing up in a system
 
-#TODO: Install aliasrc in the system by putting it in .bashrc, or something
-# similar
+function sedeasy {
+      sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
+}
 
 CURR_FILE_PATH=$(readlink -f $0)
 MYUTILS_DIR_PATH=$(echo $CURR_FILE_PATH | cut -d'/' -f-3)
 VPN_CONF_PATH=${MYUTILS_DIR_PATH}/etc/vpn.conf
-sed -i s/__VPN_CONF_FILE__/VPN_CONF_PATH/g aliasrc
+
+cp aliasrc.template ~/.aliasrc
+sedeasy "__VPN_CONF_PATH__" $VPN_CONF_PATH ~/.aliasrc
+
+IS_BASHRC_EDITED=$(cat ~/.bashrc | grep -c "source ~/.aliasrc")
+
+if [ $IS_BASHRC_EDITED -eq 0 ]; then
+    echo "source ~/.aliasrc" >> ~/.bashrc
+fi
+
+source ~/.aliasrc
