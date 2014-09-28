1d0f8537d3f869b576a22b083c01a4ba45db913d
Rushi Agrawal <rushi.agr@gmail.com>
2014-09-28 15:48:56 +0530

    Reorganize apt.sh. Make aliases more clear and consistent

--- aliases/apt.sh
+++ aliases/apt.sh
@@ -1,11 +1,13 @@
 alias au='sudo apt-get update'
 alias ai='sudo apt-get install'
-alias auy='sudo apt-get update --assume-yes'
-alias aiy='sudo apt-get install --assume-yes'
+alias aud='sudo apt-get update && sudo apt-get dist-upgrade'
 alias aac='sudo apt-get autoclean'
 alias aar='sudo apt-get autoremove'
 alias ap='sudo apt-get purge'
 alias ar='sudo apt-get remove'
 alias ad='sudo apt-get dist-upgrade'
-alias aud='sudo apt-get update --assume-yes && sudo apt-get dist-upgrade --assume-yes'
+
+alias auy='sudo apt-get update --assume-yes'
+alias aiy='sudo apt-get install --assume-yes'
+alias audy='sudo apt-get update --assume-yes && sudo apt-get dist-upgrade --assume-yes'
 
