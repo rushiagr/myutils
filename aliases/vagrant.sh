5e78f599ea4f998c1736b0d564a39e36ca3165c4
Rushi Agrawal <rushi.agr@gmail.com>
2014-11-10 15:27:55 +0530

    add vagrant aliases

---
+++ aliases/vagrant.sh
@@ -0,0 +1,7 @@
+alias vssh='vagrant ssh'
+alias vu='vagrant up'
+alias vup='vagrant up'
+alias vh='vagrant halt'
+alias vsuspend='vagrant suspend'
+alias vr='vagrant reload'
+alias vstatus='vagrant status'
