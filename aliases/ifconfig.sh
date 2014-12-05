99428bb45656d88fa99f9f7fb4623eb86bfda620
Rushi Agrawal <rushi.agr@gmail.com>
2014-12-05 23:10:09 +0530

    IFS var made ifc break sometimes, so unset it

--- aliases/ifconfig.sh
+++ aliases/ifconfig.sh
@@ -1,7 +1,8 @@
 function ifc () {
-	IFACES=$(ifconfig -s | tail -n +2 | awk '{print $1}')
-	for IF in $IFACES; do
-		echo -e $IF '\t'  $(ifconfig $IF | grep 'inet addr' | awk '{print $2}' | cut -d: -f2)
-	done
+    unset IFS
+    IFACES=$(ifconfig -s | tail -n +2 | awk '{print $1}')
+    for IF in $IFACES; do
+        echo -e $IF '\t'  $(ifconfig $IF | grep 'inet addr' | awk '{print $2}' | cut -d: -f2)
+    done
 }
 
