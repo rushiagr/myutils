ed94687df692c85c1e6ee7e034b96483cce09e9e
Rushi Agrawal <rushi.agr@gmail.com>
2014-12-05 13:19:24 +0530

    remove vpn2, vpn script takes no param now

--- aliases/vpn.sh
+++ aliases/vpn.sh
@@ -9,43 +9,7 @@ vpn () {
 
     IFS=","
 
-    if [ $# -eq 0 ] 
-    then
-        echo "usage: vpn.sh [h|o]"
-        echo "   o stands for office, h for home"
-        return
-    fi
-
-    if [ $1 == "o" ]; then
-        GATEWAY=$OFFICEGW
-    else
-        GATEWAY=$HOMEGW
-    fi
-
-    connected=`ps aux | grep openconnect | grep vpnc | wc -l`
-
-    if [ $connected -eq 0 ]; then
-        sudo openconnect -b -u $USER -s /etc/vpnc/vpnc-script $VPNIP
-        sudo route del -net 0.0.0.0 netmask 0.0.0.0 dev tun0
-        sudo route add default gw $GATEWAY
-        for ROUTE in $ROUTES; do
-            sudo route add -net $ROUTE/24 dev tun0
-        done
-    else
-        echo "Seems a VPN connection is already established. Exiting.."
-    fi
-}
-
-vpn2 () {
-    if [ ! -e ${VPN_CONF_PATH} ]; then
-        echo "No vpn.conf file found to fetch configuration from. Exiting.."
-        return
-    fi
-    source $VPN_CONF_PATH
-
-    IFS=","
-
-    GATEWAY=$(route -n | grep "^0.0.0.0" | awk '{print $1}')
+    GATEWAY=$(route -n | grep "^0.0.0.0" | awk '{print $2}')
 
     connected=`ps aux | grep openconnect | grep vpnc | wc -l`
 
