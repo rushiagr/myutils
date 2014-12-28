9a913c2e061485282de156d7af9a7b1aa793d0e8
Rushi Agrawal <rushi.agr@gmail.com>
2014-12-28 23:56:56 +0530

    Add clone-vim-plugins.sh
    clones plugins to ~/.vim/bundle dir
    
    the problem is, we'll be overwriting this file while running
    install-aliases.sh
    
    so in future, this should clone into myutils/dotfiles/.vim/bundle
    dir
    
    also maybe we'll need to handle errors with .git directory while
    running install-aliases.sh

---
+++ clone-vim-plugins.sh
@@ -0,0 +1,8 @@
+#! /bin/bash -eux
+
+cd ~/.vim/bundle
+git clone https://github.com/haya14busa/incsearch.vim
+git clone https://github.com/tomtom/tlib_vim
+git clone https://github.com/MarcWeber/vim-addon-mw-utils
+git clone https://github.com/garbas/vim-snipmate
+git clone https://github.com/nvie/vim-flake8
