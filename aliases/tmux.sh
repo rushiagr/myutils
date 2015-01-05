308ecf5332bbab86f742bcf6ff58d66f32d8e89f
Rushi Agrawal <rushi.agr@gmail.com>
2015-01-05 16:07:09 +0530

    do not set $TERM for tmux operations
    this is supplementary to previous commit

--- aliases/tmux.sh
+++ aliases/tmux.sh
@@ -1,18 +1,18 @@
-alias tmux='TERM=xterm-256color tmux'
+alias tmux='tmux'
 
 function mx() {
     if [ -z $1]; then
-        TERM=xterm-256color tmux
+        tmux
     else
-        TERM=xterm-256color tmux new -s $1
+        tmux new -s $1
     fi
 }
 
 function mxa() {
     if [ -z $1 ]; then
-        TERM=xterm-256color tmux attach
+        tmux attach
     else
-        TERM=xterm-256color tmux attach -t $1
+        tmux attach -t $1
     fi
 }
 
