1c8e16e9177e372745ee2daade0cc1343545232d
Rushi Agrawal <rushi.agr@gmail.com>
2015-06-02 17:50:13 +0530

    add -E at some more apt-get commands

--- aliases/apt.sh
+++ aliases/apt.sh
@@ -1,6 +1,6 @@
 alias au='sudo -E apt-get update --assume-yes'
 alias ai='sudo -E apt-get install --assume-yes'
-alias aud='sudo -E apt-get update --assume-yes && sudo apt-get dist-upgrade --assume-yes'
+alias aud='sudo -E apt-get update --assume-yes && sudo -E apt-get dist-upgrade --assume-yes'
 alias aac='sudo -E apt-get autoclean'
 alias aar='sudo -E apt-get autoremove'
 alias ap='sudo -E apt-get purge'
@@ -9,6 +9,6 @@ alias ad='sudo -E apt-get dist-upgrade'
 
 alias auy='sudo -E apt-get update'
 alias aiy='sudo -E apt-get install'
-alias audy='sudo -E apt-get update && sudo apt-get dist-upgrade'
+alias audy='sudo -E apt-get update && sudo -E apt-get dist-upgrade'
 
 alias dpg='dpkg -l | grep -i'
