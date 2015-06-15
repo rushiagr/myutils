c43e48dc9813d373dc6cecf0b1b4bf274db9488e
Rushi Agrawal <rushi.agr@gmail.com>
2015-06-15 10:22:38 +0530

    Make 'ad' --assume-yes, and add 'ady' for asking for 'yes/no' before dist-upgrade

--- aliases/apt.sh
+++ aliases/apt.sh
@@ -5,10 +5,11 @@ alias aac='sudo -E apt-get autoclean'
 alias aar='sudo -E apt-get autoremove'
 alias ap='sudo -E apt-get purge'
 alias ar='sudo -E apt-get remove'
-alias ad='sudo -E apt-get dist-upgrade'
+alias ad='sudo -E apt-get --assume-yes dist-upgrade'
 
 alias auy='sudo -E apt-get update'
 alias aiy='sudo -E apt-get install'
 alias audy='sudo -E apt-get update && sudo -E apt-get dist-upgrade'
+alias ady='sudo -E apt-get dist-upgrade'
 
 alias dpg='dpkg -l | grep -i'
