30fa14be606e319e692ab043613aca92ed028968
Rushi Agrawal <rushi.agr@gmail.com>
2015-07-08 10:54:31 +0530

    add pf and pfg for pip freeze and pip freeze grep

--- aliases/py.sh
+++ aliases/py.sh
@@ -7,3 +7,6 @@ alias pmr2='python manage.py runserver'
 alias pmmm2='python manage.py makemigrations'
 alias pmm2='python manage.py migrate'
 alias pm2='python manage.py'
+
+alias pf='pip freeze'
+alias pfg='pip freeze | grep -i'
