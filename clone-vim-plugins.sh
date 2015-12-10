e992ca8814dc3b7968fb6159d63cc5511a2efc75
Rushi Agrawal <rushi.agr@gmail.com>
2015-12-10 15:35:24 +0530

    add tcomment to clone-vim-plugins.sh

--- clone-vim-plugins.sh
+++ clone-vim-plugins.sh
@@ -6,3 +6,4 @@ git clone https://github.com/tomtom/tlib_vim
 git clone https://github.com/MarcWeber/vim-addon-mw-utils
 git clone https://github.com/garbas/vim-snipmate
 git clone https://github.com/nvie/vim-flake8
+git clone https://github.com/tomtom/tcomment_vim
