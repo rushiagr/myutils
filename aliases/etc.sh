206e0b79f5718b76542a12c8fb930dc2d2c47823
Rushi Agrawal <rushi.agr@gmail.com>
2016-04-06 18:35:48 +0530

    Add fixfileperms method

--- aliases/etc.sh
+++ aliases/etc.sh
@@ -217,3 +217,9 @@ function vah() {
 # In Mac, htop needs to be run as sudo to see CPU and memory usage
 alias htop='sudo htop'
 alias dk='sudo docker'
+
+# Alias to convert all files and directories to normal permissions
+function fixfileperms() {
+    find * -type d -print0 | xargs -0 chmod 0755 # for directories
+    find . -type f -print0 | xargs -0 chmod 0644 # for files
+}
