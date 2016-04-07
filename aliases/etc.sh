c7352aca9c045109c41143c10147c06b96d1aadb
Rushi Agrawal <rushi.agr@gmail.com>
2016-04-07 16:12:00 +0530

    Add gcu for git commit with 'update $(date)' as the commit message

--- aliases/etc.sh
+++ aliases/etc.sh
@@ -225,3 +225,5 @@ function fixfileperms() {
     find * -type d -print0 | xargs -0 chmod 0755 # for directories
     find . -type f -print0 | xargs -0 chmod 0644 # for files
 }
+
+alias gcu='git commit -m "update $(date)"'
