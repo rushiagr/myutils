7ad72125f72b0f5f3f1d677284feea84e8fef6b6
Rushi Agrawal <rushi.agr@gmail.com>
2016-08-10 21:46:32 +0530

    Save history after every executed command

--- aliases/bashrc.sh
+++ aliases/bashrc.sh
@@ -3,3 +3,5 @@ HISTFILESIZE=200000
 HISTCONTROL=ignoredups:erasedups:ignorespace
 HISTIGNORE="*personal*:*secret*:cd*"
 
+# After every command, save history
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
