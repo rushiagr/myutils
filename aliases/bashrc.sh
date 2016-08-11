6ec9c05ec5ec6074f230ca7f4f1bbe9d9f642029
Rushi Agrawal <rushi.agr@gmail.com>
2016-08-12 04:41:29 +0530

    Remove duplicates from history
    I can actually write a blog post on this :D

--- aliases/bashrc.sh
+++ aliases/bashrc.sh
@@ -1,7 +1,18 @@
 HISTSIZE=100000
 HISTFILESIZE=200000
-HISTCONTROL=ignoredups:erasedups:ignorespace
 HISTIGNORE="*personal*:*secret*:cd*"
 
-# After every command, save history
-export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+# Following lines do this:
+#  - If the same command is entered again consecutively, store only one copy
+#  - Save history after every command, so that even if the terminal
+#    crashes/force-killed you have saved history. Note that the side-effect of
+#    this is that if you execute a command in a terminal, it will be visible in
+#    another simultaneously running terminal's history as soon as you execute
+#    even one command in the new terminal
+#  - If a command is entered which was previously typed in the past (but not
+#    consecutively), remove all previous instances from history and add only one
+#    new instance, effectively removing all duplicates from history, including
+#    non-consecutive ones
+HISTCONTROL=ignoredups:erasedups
+shopt -s histappend
+PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
