e7dfbc25292fe1a6751ccaef5280c4f089666bf3
Rushi Agrawal <rushi.agr@gmail.com>
2018-08-03 23:13:52 +0530

    Improved 'mxa'
    and a few notes

--- superutils/safe/tmux.sh
+++ superutils/safe/tmux.sh
@@ -7,20 +7,24 @@ function mx() {
 }
 
 function mxa() {
-# Shortcut for 'tmux attach'. If an argument is provided, it attaches to that
-# specific session name. If no tmux server is running, it creates one. If an
-# argument is provided and no tmux session exists, a new session is created
-# with this name
-    if [ -z $(ps aux | grep \ Ss\ | grep tmux) ]; then
-        if [ -z $1 ]; then
-            tmux -2
+    # if arg provided
+    if [[ ! -z $1 ]]; then  # If arg provided
+        # if session with same name doesn't exist
+        if [[ $(tmux list-sessions | cut -d':' -f1 | grep -c $1) -eq 0 ]]; then
+            echo "creating new session $1 ..."
+            tmux -2 new -s $1
         else
+            tmux -2 attach -t $1
+        fi
+    else
+        # if tmux is running
+        if [[ ! -z $(ps aux | grep \ Ss\ | grep tmux) ]]; then
+            tmux -2 attach
+        else
+            echo "tmux wasn't running. Creating new session without a name..."
+            sleep 1
             tmux -2 new -s $1
         fi
-    elif [ -z $1 ]; then
-        tmux -2 attach
-    else
-        tmux -2 attach -t $1
     fi
 }
 
