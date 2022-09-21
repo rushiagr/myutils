9254f9c328dacddc5f6993e0ae56f846111ad3b8
Rushi Agrawal <rushi.agr@gmail.com>
2022-09-22 00:52:34 +0530

    add comments to make code more readable

--- unsafe/zsh-specific.sh
+++ unsafe/zsh-specific.sh
@@ -1,3 +1,4 @@
+# case-insensitive autocomplete
 autoload -Uz compinit && compinit
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 
@@ -5,7 +6,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
 export HISTFILESIZE=1000000
 export HISTSIZE=1000000
 export HISTFILE=~/.zsh_history
-
 setopt HIST_FIND_NO_DUPS
 # following should be turned off, if sharing history via setopt SHARE_HISTORY
 setopt INC_APPEND_HISTORY
