2fd8248cd4265915e19dfb4b922fdd81a5e1180b
Rushi Agrawal <rushi.agr@gmail.com>
2022-09-22 00:20:13 +0530

    Just keep one install shell script
    because likely bash one is broken

--- install-aliases.sh
+++ install-aliases.sh
@@ -1,53 +1,54 @@
-#! /bin/bash -eu
+#! /bin/zsh -eu
 
 # Script which sets everthing up in a system
 
+
+UNAME=$(uname)
+
 function sedeasy {
-    if [ $(uname) == "Darwin" ]; then
+    if [[ $UNAME == "Darwin" ]]; then
           sed -i "" "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
       else
         sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
     fi
 }
 
-BASHRC_FILE="$HOME/.bashrc"
+ZSHRC_FILE="$HOME/.zshrc"
 
 
-if [ $(uname) == "Darwin" ]; then
-    if [ $(brew list | grep -c coreutils) == "0" ]; then
+if [[ $UNAME == "Darwin" ]]; then
+    if [[ $(brew list | grep -c coreutils) == "0" ]]; then
         echo "'coreutils' brew package not installed. Installing now..."
         brew install coreutils
     fi
     CURR_FILE_PATH=$(greadlink -f $0)
-    BASHRC_FILE="$HOME/.bash_profile"
+    ZSHRC_FILE="$HOME/.zprofile"
 else
     CURR_FILE_PATH=$(readlink -f $0)
 fi
+
 MYUTILS_DIR_PATH=$(echo $CURR_FILE_PATH | rev | cut -d'/' -f2- | rev)
-#VPN_CONF_PATH=${MYUTILS_DIR_PATH}/etc/vpn.conf
 
-cat ${MYUTILS_DIR_PATH}/aliases/* >  $HOME/.aliasrc
 cat ${MYUTILS_DIR_PATH}/superutils/safe/* >>  $HOME/.aliasrc
-#sedeasy "__VPN_CONF_PATH__" $VPN_CONF_PATH $HOME/.aliasrc
 
-export IS_BASHRC_EDITED=$(cat $BASHRC_FILE | grep -c "source $HOME/.aliasrc")
+export IS_ZSHRC_EDITED=$(cat $ZSHRC_FILE | grep -c "source $HOME/.aliasrc")
 
-if [ $IS_BASHRC_EDITED -eq 0 ]; then
-    echo "source $HOME/.aliasrc" >> $BASHRC_FILE
+if [[ $IS_ZSHRC_EDITED -eq 0 ]]; then
+    echo "source $HOME/.aliasrc" >> $ZSHRC_FILE
 fi
 
-DOTFILES=$(ls -a $MYUTILS_DIR_PATH/dotfiles | grep ^\\.[a-zA-Z])
+DOTFILES=$(ls -a $MYUTILS_DIR_PATH/dotfiles | grep "^\\.[a-zA-Z]")
 
 TEMPDIR=""
-for DOTFILE in $DOTFILES; do
-    if [ -f $HOME/${DOTFILE} ]; then
-        if [ "${TEMPDIR}" == "" ]; then
+for DOTFILE in $(echo $DOTFILES); do
+    if [[ -f $HOME/${DOTFILE} ]]; then
+        if [[ "${TEMPDIR}" == "" ]]; then
             TEMPDIR=$(mktemp -d /tmp/myutils-backup-$(date +"%y%m%d%H%M%S")-XXXX)
         fi
         mv $HOME/$DOTFILE $TEMPDIR
     fi
 
-    if [ $(uname) == "Darwin" ]; then
+    if [[ $UNAME == "Darwin" ]]; then
         gcp --recursive $MYUTILS_DIR_PATH/dotfiles/$DOTFILE $HOME
     else
         cp --recursive $MYUTILS_DIR_PATH/dotfiles/$DOTFILE $HOME
@@ -58,7 +59,7 @@ echo "Install successful."
 echo "Aliases will be sourced from the next new shell session. To source them in the current session, do"
 echo "    source ~/.aliasrc"
 
-if [ "${TEMPDIR}" != "" ]; then
+if [[ "${TEMPDIR}" != "" ]]; then
     echo -e "\nThe following existing dotfiles are moved to temp dir $TEMPDIR:"
     echo $(ls -a $TEMPDIR | grep ^\\.[a-zA-Z])
 fi
@@ -75,7 +76,7 @@ fi
 
 vim_plugins_to_download=0
 cd ~/.vim/bundle
-for repo_url in $(cat $MYUTILS_DIR_PATH/vim-plugins | grep -v "^#" | grep -v "^$"); do
+for repo_url in $(cat $MYUTILS_DIR_PATH/vim-plugins); do
     plugin_name=$(echo $repo_url | rev | cut -d/ -f1 | rev)
     if [[ $(ls ~/.vim/bundle/ | grep -c $plugin_name$) == 0 ]]; then
         if [[ $vim_plugins_to_download == 0 ]]; then
